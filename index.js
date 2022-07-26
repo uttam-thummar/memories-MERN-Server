5bd0685e039b1c5e686fd5dac589cc26c4f56450
uttam-thummar <uttamthummar.mscit19@vnsgu.ac.in>
2022-07-26 12:51:34 +0530

    Feat: Implemented fully functional CRUD for memories. So far without authentication.
    Closes [26-07-2022 12:30 PM]

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js'
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+// https://www.mongodb.com/atlas/database
+// const CONNECTION_URL = "mongodb+srv://memories-own:0ETVky95qhfPQclp@cluster0.dskaw.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5005;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
